c67fcad0dbd73ccfe7f9b3a080c289767a7c586e
Katrina Pingryn <katep@indeed.com>
2024-02-12 12:33:01 -0600

    add users controller and router

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -5,6 +5,68 @@ async function list(req, res) {
   res.send(users);
 }
 
+async function userExists(req, res, next) {
+  const { userId } = req.params;
+  const foundUser = await User.findOne({ _id: userId });
+  if (foundUser) {
+    res.locals.user = foundUser;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `User id not found: ${userId}`,
+  });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.user });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`,
+    });
+  };
+}
+
+async function create(req, res) {
+  const { data: { username, email } = {} } = req.body;
+  const newUser = new User({
+    username: username,
+    email: email,
+  });
+  await newUser.save();
+  res.status(201).json({ data: newUser });
+}
+
+async function update(req, res) {
+  const user = res.locals.user;
+  const { data: { username, email } = {} } = req.body;
+
+  user.username = username;
+  user.email = email;
+  await user.save();
+
+  res.json({ data: user });
+}
+
+async function destroy(req, res) {
+  const { userId } = req.params;
+  await User.deleteOne({ _id: userId });
+  res.sendStatus(204);
+}
+
 module.exports = {
   list,
+  read: [userExists, read],
+  create: [bodyDataHas("username"), bodyDataHas("email"), create],
+  update: [userExists, bodyDataHas("username"), bodyDataHas("email"), update],
+  delete: [userExists, destroy],
+  userExists,
 };
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -2,6 +2,17 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./users.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router
+  .route("/:userId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
